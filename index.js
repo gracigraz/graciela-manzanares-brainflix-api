527d23cd336d3c4aefed79dc9828873731c84942
Graciela Manzanares <gracielamanzanares@MacBook-Pro.localdomain>
2023-08-08 15:20:25 -0400

    setting up server and routes (in progress)

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const express = require('express');
+
+//import required modules to create a server that handles api reqeust and serves static files
+const express = require('express'); //create an express app, used to create a server and handle http requests
 const app = express(); //is an instance of express
+require('dotenv').config();
+// const PORT = 8081; //port number on which the server will listen
+// const videosRoutes = require('./routes/videosRoutes') //module that defined routes related to videos.
+const cors = require('cors') //this module enables Cross-Origin Resourse Sharing to allow different origins (domains) to access our server resources
+
+
+// require('dotenv').config()
+//use port 5051 unless PORT is set in .env
+const PORT =process.env.PORT;
+
+
+// app.use(cors()) enable cors, this configures the CORS middleware to allow requests from any origin (*)
+app.use(cors({
+    origin: '*',
+})
+)
+
+//This middleware parses incoming JSON data from requests and makes it available in req.body for easy access.
+app.use(express.json());
+
+
+//express static is a middleware function used to configure how express should handle 
+//static files, this way we don't need to manually handle routing and serving of each static file in the app
+app.use('/public-images', express.static('./public/images'));
 
 //configure route, the path is the route at which the callback function will be invoked
 // GET method route
-app.get('/', function(req, res){
-    res.send('GET request to/Welcome to /');
+app.get('/', (req, res)=>{
+    console.log('Received a get request');
+    res.send('GET request to/Welcome to /')
 });
 
-//listen for connection on the given path (all paths on port 8080)
-app.listen(8080, function(){
-    console.log('Hello');
+//use the routes defined in the videosRoutes module under '/videos' route. 
+// app.use('/videos', videosRoutes);
+
+
+//start the server - listen for connection on the given path (all paths on port 8080)
+app.listen(PORT, ()=>{
+    console.log('Server in running on PORT ' + PORT);
 });
---
+++ routes/videos.js
@@ -0,0 +1,8 @@
+//import required modules
+const express = require('express') //express to create the route
+const router = express.Router() //router to create an instance of an express router used to define the routes
+const fs = require('fs') //fs is the file system modules to read and wrtie files
+
+
+
+module.exports = router; //Export the router so that it can be used in other parts of the application
