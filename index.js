88ded871f79de4f33009ab93afef62d8c8a266bf
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-08-08 23:30:02 -0400

    setting up routes and functionality

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
 //import required modules to create a server that handles api reqeust and serves static files
+
 const express = require('express'); //create an express app, used to create a server and handle http requests
 const app = express(); //is an instance of express
+
 require('dotenv').config();
-// const PORT = 8081; //port number on which the server will listen
-// const videosRoutes = require('./routes/videosRoutes') //module that defined routes related to videos.
-const cors = require('cors') //this module enables Cross-Origin Resourse Sharing to allow different origins (domains) to access our server resources
-
-
-// require('dotenv').config()
-//use port 5051 unless PORT is set in .env
 const PORT =process.env.PORT;
+
+const videos = require('./routes/videos') //module that defined routes related to videos.
+const cors = require('cors') //this module enables Cross-Origin Resourse Sharing to allow different origins (domains) to access our server resources
 
 
 // app.use(cors()) enable cors, this configures the CORS middleware to allow requests from any origin (*)
@@ -31,14 +28,14 @@ app.use('/public-images', express.static('./public/images'));
 // GET method route
 app.get('/', (req, res)=>{
     console.log('Received a get request');
-    res.send('GET request to/Welcome to /')
+    res.send('GET request to.. the server is running')
 });
 
 //use the routes defined in the videosRoutes module under '/videos' route. 
-// app.use('/videos', videosRoutes);
+app.use('/videos', videos);
 
 
 //start the server - listen for connection on the given path (all paths on port 8080)
 app.listen(PORT, ()=>{
-    console.log('Server in running on PORT ' + PORT);
+    console.log('Server running on PORT ' + PORT);
 });
--- routes/videos.js
+++ routes/videos.js
@@ -3,6 +3,43 @@ const express = require('express') //express to create the route
 const router = express.Router() //router to create an instance of an express router used to define the routes
 const fs = require('fs') //fs is the file system modules to read and wrtie files
 
+function readVideosFile() {
+	const videoList = fs.readFileSync('./data/videos.json')
+	const parsedData = JSON.parse(videoList)
+	return parsedData
+}
 
+//use the logMessage middleware for all routes - every request that matches any route defined after 
+//router.use(logMessage) will first pass through the logMessage middleware, which logs the request details, 
+//and then proceed to the appropriate route handler or middleware. 
+router.use(logMessage)
+
+// Define a route handler for the root URL ("/videos")
+router.get('/', (req, res) => {
+    // Read videos data from a file (synchronous read for simplicity)
+	const videos = readVideosFile() 
+	res.json(videos) //If the reading is successful, the videos data is sent as a JSON response
+	res.end()
+})
+
+
+// GET endpoint for an individual video
+router.get("/:videoId", (req, res) => {
+    // Read the file and find the single video whose id matches the requested id
+    const videos = readVideosFile();
+    const singleVideo = videos.find((video) => video.id === req.params.videoId);
+
+    // This might be a good place to check if the athlete was found 👀
+
+    // Respond with the single video
+    res.json(singleVideo);
+});
+
+
+
+function logMessage(req, res, next) {
+	console.log('hello from the videos route');
+	next()
+}
 
 module.exports = router; //Export the router so that it can be used in other parts of the application
