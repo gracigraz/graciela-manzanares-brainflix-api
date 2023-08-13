f099a8fdb5eca064f1dd6bf9fac3ec05133b41b7
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-08-12 22:19:51 -0400

    cleaned up index.js and videos.js files

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 //import required modules to create a server that handles api reqeust and serves static files
 
-const express = require('express'); //create an express app, used to create a server and handle http requests
+const express = require("express"); //create an express app, used to create a server and handle http requests
 const app = express(); //is an instance of express
 
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT;
 
-const videos = require('./routes/videos') //module that defined routes related to videos.
-const cors = require('cors') //this module enables Cross-Origin Resourse Sharing to allow different origins (domains) to access our server resources
-
+const videos = require("./routes/videos"); //module that defined routes related to videos.
+const cors = require("cors"); //this module enables Cross-Origin Resourse Sharing to allow different origins (domains) to access our server resources
 
 // app.use(cors()) enable cors, this configures the CORS middleware to allow requests from any origin (*)
-app.use(cors({
-    origin: '*',
-})
-)
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //This middleware parses incoming JSON data from requests and makes it available in req.body for easy access.
 app.use(express.json());
 
-
-//express static is a middleware function used to configure how express should handle 
+//express static is a middleware function used to configure how express should handle
 //static files, this way we don't need to manually handle routing and serving of each static file in the app
-app.use('/public-images', express.static('./public/images'));
+app.use("/public-images", express.static("./public/images"));
 
 //configure route, the path is the route at which the callback function will be invoked
 // GET method route
-app.get('/', (req, res) => {
-    console.log('Received a get request');
-    res.send('GET request to.. the server is running')
+app.get("/", (req, res) => {
+  res.send("GET request to.. the server is running");
 });
 
-//use the routes defined in the videosRoutes module under '/videos' route. 
-app.use('/videos', videos);
-
+//use the routes defined in the videos route module under '/videos' route.
+app.use("/videos", videos);
 
 //start the server - listen for connection on the given path (all paths on port 8080)
 app.listen(PORT, () => {
-    console.log('Server running on PORT ' + PORT);
+  console.log("Server running on PORT " + PORT);
 });
--- routes/videos.js
+++ routes/videos.js
@@ -1,85 +1,110 @@
 //import required modules
-const express = require('express') //express to create the route
-const router = express.Router() //router to create an instance of an express router used to define the routes
-const fs = require('fs') //fs is the file system modules to read and wrtie files
-const crypto = require('crypto') //import the built-in crypto module, to generate secure random number for unique video ID req
+const express = require("express"); //express to create the route
+const router = express.Router(); //router to create an instance of an express router used to define the routes
+const fs = require("fs"); //fs is the file system module to read and wrtie files
+const crypto = require("crypto"); //import the built-in crypto module, to generate secure random number for unique video ID req
 
 function readVideosFile() {
-    console.log('gi3')
-	const videoList = fs.readFileSync('./data/videos.json');
-	const parsedData = JSON.parse(videoList);
-	return parsedData;
+  const videoList = fs.readFileSync("./data/videos.json");
+  const parsedData = JSON.parse(videoList);
+  return parsedData;
 }
 
-//use the logMessage middleware for all routes - every request that matches any route defined after 
-//router.use(logMessage) will first pass through the logMessage middleware, which logs the request details, 
-//and then proceed to the appropriate route handler or middleware. 
+//use the logMessage middleware for all routes
 router.use(logMessage);
 
 // Define a route handler for the root URL ("/videos")
-router.get('/', (req, res) => {
-    // console.log(req)
-    //Read videos data from a file (synchronous read for simplicity)
-	const videos = readVideosFile(); 
-    // console.log(res)
-	res.json(videos) //If the reading is successful, the videos data is sent as a JSON response
-	res.end()
-})
-
+router.get("/", (req, res) => {
+  //Read videos data from a file (synchronous read for simplicity)
+  const videos = readVideosFile();
+  res.json(videos); //If the reading is successful, the videos data is sent as a JSON response
+  res.end();
+});
 
 // GET endpoint for an individual video
 router.get("/:id", (req, res) => {
-    // Read the file and find the single video whose id matches the requested id
-    const videos = readVideosFile();
-    // console.log(videos)
-    // console.log(req.params)
-    const singleVideo = videos.find((video) => video.id === req.params.id);
-    console.log('this is ' +singleVideo)
-    //check if the video was found 👀
-
-    // Respond with the single video
-    res.json(singleVideo);
+  // Read the file and find the single video whose id matches the requested id
+  const videos = readVideosFile();
+  const singleVideo = videos.find((video) => video.id === req.params.id);
+  // Respond with the single video
+  res.json(singleVideo);
 });
 
-// POST endpoint to add a new video to the list. 
-//A unique id must be negerated for eacg video added
-router.post("/", (req, res) => {    
-    // Make a new video with a unique id
-	console.log( 'this is a r' +req);
-    console.log(req.body);
-    let randomCommentNum = Math.floor(Math.random() * 4);
-    let randomLikes = Math.floor(Math.random() * 1000000) + 1000;
-    let randomViews = Math.floor(Math.random() * 1000000) + 1000;
-    const newVideo = {
+// POST endpoint to add a new video to the list. A unique id must be negerated for eacg video added.
+router.post("/", (req, res) => {
+  // Make a new video with a unique id
+
+  let randomLikes = (Math.floor(Math.random() * 1000000) + 1000).toLocaleString(
+    "en-US"
+  );
+
+  let randomViews = (Math.floor(Math.random() * 1000000) + 1000).toLocaleString(
+    "en-US"
+  );
+
+  let randomDate = new Date(
+    Math.random() * new Date().getTime()
+  ).toLocaleDateString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  });
+
+  generateRandomText = (numWords) => {
+    const loremWords =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const loremText = loremWords.split(" ");
+    const randomText = [];
+    while (randomText.length < numWords) {
+      const randomIndex = Math.floor(Math.random() * loremText.length);
+      randomText.push(loremText[randomIndex]);
+    }
+    return randomText.join(" ");
+  };
+  const generateRandomComments = () => {
+    let comments = [];
+    let numComments = Math.floor(Math.random() * 4) + 1;
+    for (let i = 0; i < numComments; i++) {
+      let obj = {
         id: crypto.randomUUID(),
-        title: req.body.title,
-        channel: `channel of ` + req.body.title,
-        description: req.body.description,
-        image: "http://localhost:8082/public-images/Upload-video-preview.jpg",
-        views: { randomViews },
-        likes: { randomLikes },
-        comments: { randomCommentNum },
-        timestamp: new Date()
-    };
+        name: generateRandomText(2),
+        comment: generateRandomText(30),
+        timestamp: new Date(
+          Math.random() * new Date().getTime()
+        ).toLocaleDateString("en-US", {
+          month: "2-digit",
+          day: "2-digit",
+          year: "numeric",
+        }),
+      };
+      comments.push(obj);
+    }
+    return comments;
+  };
+  //new video with unique id
+  const newVideo = {
+    id: crypto.randomUUID(),
+    title: req.body.title,
+    channel: generateRandomText(3),
+    image: "http://localhost:8082/public-images/Upload-video-preview.jpg",
+    description: req.body.description,
+    views: randomViews,
+    likes: randomLikes,
+    timestamp: randomDate,
+    comments: generateRandomComments(),
+  };
 
-    // 1. Read the current videos array
-    // 2. Add to the videos array
-    // 3. Write the entire new videos array to the file
-    const videos = readVideosFile();
-    videos.push(newVideo);
-    fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+  // Read the current videos array, add to the videos array, write the entire new videos array to the file
+  const videos = readVideosFile();
+  videos.push(newVideo);
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
 
-    // Respond with the video that was created
-    res.status(201).json(newVideo);
+  // Respond with the video that was created
+  res.status(201).json(newVideo);
 });
-
-
-
-
 
 function logMessage(req, res, next) {
-	console.log('hello from the videos route');
-	next()
+  next();
 }
 
 module.exports = router; //Export the router so that it can be used in other parts of the application
